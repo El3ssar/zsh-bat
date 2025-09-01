85bfe87ec8a65b9bf4170087b9e69e08a00bba70
Daniel Estevez <kemossabee@gmail.com>
2025-09-01 11:02:17 +0200

    Add the GitHub Dark theme by default to bat

--- zsh-bat.plugin.zsh
+++ zsh-bat.plugin.zsh
@@ -1,19 +1,19 @@
-
 if command -v batcat >/dev/null 2>&1; then
   # Save the original system `cat` under `rcat`
   alias rcat="$(which cat)"
 
   # For Ubuntu and Debian-based `bat` packages
   # the `bat` program is named `batcat` on these systems
-  alias cat="$(which batcat)"
-  export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
+  alias cat="batcat --plain --theme='GitHub Dark'"
+  export MANPAGER="sh -c 'col -bx | batcat --theme=\"GitHub Dark\" -l man -p'"
   export MANROFFOPT="-c"
 elif command -v bat >/dev/null 2>&1; then
   # Save the original system `cat` under `rcat`
   alias rcat="$(which cat)"
 
   # For all other systems
-  alias cat="$(which bat)"
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  alias cat="bat --plain --theme='GitHub Dark'"
+  export MANPAGER="sh -c 'col -bx | bat --theme=\"GitHub Dark\" -l man -p'"
   export MANROFFOPT="-c"
 fi
+
